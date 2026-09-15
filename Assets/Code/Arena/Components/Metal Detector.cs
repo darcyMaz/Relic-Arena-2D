77ecb2afdae21ef2ca39d7a8ff9ee133adb9e6fd
Darcy Mazloum <92594097+darcyMaz@users.noreply.github.com>
2026-09-15 12:18:36 -0400

    2D Project Deleted

--- Assets/Code/Arena/Components/Metal Detector.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class MetalDetector : MonoBehaviour
-{
-    // the metal detector compares the transform with the arena's active buried relics
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-Relic Arena Game
-
-![Class Diagram](https://github.com/darcyMaz/Relic-Arena-2D/blob/master/RelicArenaUML_no_bg.png)
