95d6deb0cd4e93aca3c8b685f748005dcb94dd2f
Darcy Mazloum <92594097+darcyMaz@users.noreply.github.com>
2026-09-18 11:15:33 -0400

    Update code from other 3d project and input manager a bit

--- Assets/Code/Player/InputMovement2D.cs
+++ Assets/Code/Player/InputMovement2D.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class InputMovement2D : MonoBehaviour
 {
     [SerializeField] private float Speed = 2f;
 
--- Assets/Code/Player/Player.cs
+++ Assets/Code/Player/Player.cs
@@ -3,19 +3,54 @@ using UnityEngine;
 
 public class Player : IEffectable
 {
+    /// <summary>
+    /// The Player's number, set in the inspector.
+    /// </summary>
     [SerializeField] private int PlayerNumber = 0;
 
-    // The Player is composed of the following components.
+    /// <summary>
+    /// The Player's Inventory.
+    /// </summary>
     private Inventory _inventory;
     private bool _hasInventory = false;
     
+    /// <summary>
+    /// The Player's Metal Detector.
+    /// </summary>
     private MetalDetector _metalDetector;
     private bool _hasMetalDetector = false;
 
-    private Rigidbody2D _rigidBody;
-    private bool _hasRigidBody = false;
-
+    /// <summary>
+    /// Initialize the components of the Player.
+    /// </summary>
     private void Start()
+    {
+        StartInit();
+    }
+
+    /// <summary>
+    /// Get this Player's number.
+    /// </summary>
+    /// <returns> The Player's number as an integer. </returns>
+    public int GetPlayerNumber()
+    {
+        return PlayerNumber;
+    }
+
+    private void RelicHitPlayer()
+    {
+        // When the player gets hit by an item.
+        // Need specification.
+        // Does the player lose all of their items?
+        // Lose (idk) 40% at random?
+
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// The method holding all of the initializations that the Player component must do at the Start function. 
+    /// </summary>
+    private void StartInit()
     {
         // The Player has many components, try to find them and get them.
         if (TryGetComponent(out _inventory))
@@ -34,34 +69,24 @@ public class Player : IEffectable
         {
             Debug.Log("Player #" + PlayerNumber + " does not have a Metal Detector component. The game will still work but the player will not be able to find items.");
         }
-        if (TryGetComponent(out _rigidBody))
-        {
-            _hasRigidBody = true;
-        }
-        else
-        {
-            Debug.Log("Player #" + PlayerNumber + " does not have a Rigidbody2D component. The game will still work but the player will not be able to move.");
-        }
     }
 
-    public int GetPlayerNumber()
+    /// <summary>
+    /// Applies the knockout effect.
+    /// </summary>
+    /// <param name="knockoutTime"> Amount of time to be knocked out. </param>
+    protected override void ApplyKnockout(float knockoutTime)
     {
-        return PlayerNumber;
+        Debug.Log("Knockout Effect not implemented onto the Player.");
     }
 
-    private void RelicHitPlayer()
+    protected override void ApplyChangeSpeed(float speed, float duration)
     {
-        // When the player gets hit by an item.
-        // Need specification.
-        // Does the player lose all of their items?
-        // Lose (idk) 40% at random?
-
-        throw new NotImplementedException();
+        Debug.Log("ChangeSpeed Effect nto implemented by Player.");
     }
 
-    private void EffectRelicHitPlayer()
+    protected override void ApplyLightning()
     {
-        throw new NotImplementedException();
+        Debug.Log("A Player was struck by lightning but the effect is not applied to the Player yet.");
     }
-
 }
